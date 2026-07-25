950cdbf0857760c40074709ac11496ec48310961
gelsavio <gelsavio@gmail.com>
2026-07-25 19:07:34 -0300

    permite sequenciar notas naturais para solos

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const CACHE_PREFIX = 'teclado-virtual-pwa-';
-const CACHE_NAME = CACHE_PREFIX + 'v3.11.1';
+const CACHE_NAME = CACHE_PREFIX + 'v3.12.3';
 const OFFLINE_URL = './offline.html';
 
 const PRECACHE_URLS = [
