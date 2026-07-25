0493131e1f00bd158e23ce906ca614c504689b26
gelsavio <gelsavio@gmail.com>
2026-07-25 15:19:21 -0300

    aumentadas as sequencias para 16

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const CACHE_PREFIX = 'teclado-virtual-pwa-';
-const CACHE_NAME = CACHE_PREFIX + 'v3.10.2';
+const CACHE_NAME = CACHE_PREFIX + 'v3.11.1';
 const OFFLINE_URL = './offline.html';
 
 const PRECACHE_URLS = [
