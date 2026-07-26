a11e42e026024b059afba6477e49a55784a1b2b1
gelsavio <gelsavio@gmail.com>
2026-07-26 10:34:52 -0300

    permitido escolher instrumento por seção

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const CACHE_PREFIX = 'teclado-virtual-pwa-';
-const CACHE_NAME = CACHE_PREFIX + 'v3.12.3';
+const CACHE_NAME = CACHE_PREFIX + 'v3.13.2';
 const OFFLINE_URL = './offline.html';
 
 const PRECACHE_URLS = [
